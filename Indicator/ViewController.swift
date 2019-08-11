84d841aa6edabcafd602efa365232564f9b59101
GumpWang <1989wg@gmail.com>
2019-08-12 00:17:38 +0800

    Initial Commit

---
+++ Indicator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Indicator
+//
+//  Created by gang on 2019/8/12.
+//  Copyright © 2019 Gump. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
